5d2c7ea791cb378d5356d5eeb5519f8ddb0fc48b
Yann Renaudin <renaudin.yann@gmail.com>
2018-11-13 23:33:47 -0500

    feat: first stable implementation for responses interceptor with url matcher

---
+++ src/AxiosMorphism.spec.ts
@@ -0,0 +1,224 @@
+import { combine, AxiosMorphismConfiguration, apply } from './AxiosMorphism';
+import axios, { AxiosInstance } from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { StrictSchema } from 'morphism';
+
+interface People {
+  name: string;
+  height: string;
+  mass: string;
+}
+interface Planet {
+  name: string;
+  climate: string;
+}
+interface Starship {
+  name: string;
+  model: string;
+  length: string;
+}
+describe('Axios Morphism', () => {
+  describe('Combine', () => {
+    it('should return a Root Configuration after using combine', () => {
+      const baseURL = 'http://api.com';
+      const expected: AxiosMorphismConfiguration = {
+        url: baseURL,
+        interceptors: {
+          requests: [],
+          responses: []
+        }
+      };
+      expect(combine(baseURL)).toEqual(expected);
+    });
+    it('should combine multiple configurations', () => {
+      const subset1: AxiosMorphismConfiguration = {
+        url: '/people',
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/', schema: { name: 'name', url: 'url' }, dataSelector: 'results' },
+            { matcher: '/:id', schema: { name: 'name', url: 'url' }, dataSelector: 'results' }
+          ]
+        }
+      };
+      const subset2: AxiosMorphismConfiguration = {
+        url: '/planets',
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/', schema: { name: 'name', url: 'url' }, dataSelector: 'results' },
+            { matcher: '/:id', schema: { name: 'name', url: 'url' }, dataSelector: 'results' }
+          ]
+        }
+      };
+
+      const expected: AxiosMorphismConfiguration = {
+        url: 'https://swapi.co/api',
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/people', schema: { name: 'name', url: 'url' }, dataSelector: 'results' },
+            { matcher: '/people/:id', schema: { name: 'name', url: 'url' }, dataSelector: 'results' },
+            { matcher: '/planets', schema: { name: 'name', url: 'url' }, dataSelector: 'results' },
+            { matcher: '/planets/:id', schema: { name: 'name', url: 'url' }, dataSelector: 'results' }
+          ]
+        }
+      };
+
+      expect(combine('https://swapi.co/api', subset1, subset2)).toEqual(expected);
+    });
+  });
+  describe('Apply', () => {
+    let client: AxiosInstance;
+    let mock: MockAdapter;
+    const baseURL = 'https://swapi.co/api';
+    const mockPeople = {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      hair_color: 'blond',
+      skin_color: 'fair'
+    };
+    const mockPlanet = {
+      name: 'Tatooine',
+      climate: 'arid',
+      rotation_period: '23',
+      orbital_period: '304'
+    };
+    const mockStarship = {
+      name: 'Death Star',
+      model: 'DS-1 Orbital Battle Station',
+      manufacturer: 'Imperial Department of Military Research, Sienar Fleet Systems',
+      cost_in_credits: '1000000000000',
+      length: '120000'
+    };
+    const expectedPeople: People = {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77'
+    };
+    const expectedPlanet: Planet = {
+      name: 'Tatooine',
+      climate: 'arid'
+    };
+    const expectedStarship: Starship = {
+      name: 'Death Star',
+      model: 'DS-1 Orbital Battle Station',
+      length: '120000'
+    };
+
+    const peopleSchema: StrictSchema<People> = { name: 'name', height: 'height', mass: 'mass' };
+    const planetSchema: StrictSchema<Planet> = { name: 'name', climate: 'climate' };
+    const starshipSchema: StrictSchema<Starship> = { name: 'name', model: 'model', length: 'length' };
+
+    beforeEach(() => {
+      client = axios.create();
+      mock = new MockAdapter(client);
+
+      // People API
+      mock.onGet(`${baseURL}/people`).reply(200, { results: [mockPeople] });
+      mock.onGet(`${baseURL}/people/id`).reply(200, mockPeople);
+      mock.onGet(`${baseURL}/people/id/starships/id`).reply(200, mockStarship);
+
+      //Planets API
+      mock.onGet(`${baseURL}/planets`).reply(200, { results: [mockPlanet] });
+      mock.onGet(`${baseURL}/planets/id`).reply(200, mockPlanet);
+
+      // Starships API
+      mock.onGet(`${baseURL}/starships`).reply(200, { results: [mockStarship] });
+      mock.onGet(`${baseURL}/starships/id`).reply(200, mockStarship);
+    });
+    afterEach(() => {
+      mock.reset();
+    });
+
+    it('should apply response interceptors on flat urls (e.g /people, /planets) and use dataSelector when present to access axios data', async () => {
+      const config: AxiosMorphismConfiguration = {
+        url: baseURL,
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/people', schema: peopleSchema, dataSelector: 'results' },
+            { matcher: '/people/:id', schema: peopleSchema },
+            { matcher: '/planets', schema: planetSchema, dataSelector: 'results' }
+          ]
+        }
+      };
+      apply(client, config);
+
+      const planets = await client.get(`${baseURL}/planets`);
+      const people = await client.get(`${baseURL}/people`);
+      expect(planets.data.results).toEqual([expectedPlanet]);
+      expect(people.data.results).toEqual([expectedPeople]);
+    });
+
+    it('should apply response interceptors on nested urls (e.g /people/:id)', async () => {
+      const config: AxiosMorphismConfiguration = {
+        url: baseURL,
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/people', schema: peopleSchema, dataSelector: 'results' },
+            { matcher: '/people/:id', schema: peopleSchema }
+          ]
+        }
+      };
+      apply(client, config);
+
+      const people = await client.get(`${baseURL}/people`);
+      const aPerson = await client.get(`${baseURL}/people/id`);
+      expect(people.data.results).toEqual([expectedPeople]);
+      expect(aPerson.data).toEqual(expectedPeople);
+    });
+
+    it('should apply response interceptors on deeply nested urls (e.g /people/:id/starships/:id)', async () => {
+      const config: AxiosMorphismConfiguration = {
+        url: baseURL,
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/people/:id', schema: peopleSchema },
+            { matcher: '/people/:id/starships/:id', schema: starshipSchema }
+          ]
+        }
+      };
+      apply(client, config);
+
+      const aPerson = await client.get(`${baseURL}/people/id`);
+      const aStarship = await client.get(`${baseURL}/people/id/starships/id`);
+
+      expect(aPerson.data).toEqual(expectedPeople);
+      expect(aStarship.data).toEqual(expectedStarship);
+    });
+
+    it('should apply response interceptors on combined morphisms', async () => {
+      const peopleMorphism: AxiosMorphismConfiguration = {
+        url: '/people',
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/', schema: peopleSchema, dataSelector: 'results' },
+            { matcher: '/:id', schema: peopleSchema }
+          ]
+        }
+      };
+      const planetMorphism: AxiosMorphismConfiguration = {
+        url: '/planets',
+        interceptors: {
+          requests: [],
+          responses: [
+            { matcher: '/', schema: planetSchema, dataSelector: 'results' },
+            { matcher: '/:id', schema: planetSchema }
+          ]
+        }
+      };
+      apply(client, combine(baseURL, peopleMorphism, planetMorphism));
+
+      const aPerson = await client.get(`${baseURL}/people/id`);
+      const aPlanet = await client.get(`${baseURL}/planets/id`);
+
+      expect(aPerson.data).toEqual(expectedPeople);
+      expect(aPlanet.data).toEqual(expectedPlanet);
+    });
+  });
+});
---
+++ src/AxiosMorphism.ts
@@ -0,0 +1,95 @@
+import { morphism, Schema, StrictSchema } from 'morphism';
+import { AxiosResponse, AxiosInstance, AxiosRequestConfig } from 'axios';
+import * as urljoin from 'url-join';
+import * as pathToRegexp from 'path-to-regexp';
+
+type ResponseMatcherFunction = (response: AxiosResponse) => boolean;
+type RequestMatcherFunction = (request: AxiosRequestConfig) => boolean;
+
+interface ResponseMatcher {
+  matcher: string | RegExp | ResponseMatcherFunction;
+  schema: Schema<any> | StrictSchema<any>;
+  dataSelector?: string;
+}
+interface RequestMatcher {
+  matcher: string | RegExp | RequestMatcherFunction;
+  schema: Schema<any> | StrictSchema<any>;
+  dataSelector?: string;
+}
+export interface AxiosMorphismConfiguration {
+  url: string;
+  interceptors: {
+    requests: RequestMatcher[];
+    responses: ResponseMatcher[];
+  };
+}
+
+export function combine(baseURL: string, ...configurations: AxiosMorphismConfiguration[]) {
+  const initialValue: AxiosMorphismConfiguration = { url: baseURL, interceptors: { requests: [], responses: [] } };
+  return configurations.reduce((rootConfiguration, configuration) => {
+    const configurationsMatcherString = configuration.interceptors.responses
+      .filter(c => typeof c.matcher === 'string')
+      .map(config => ({ ...config, matcher: urljoin(configuration.url, (<string>config.matcher).replace(/\/$/, '')) }));
+
+    const configurationsMatcherFunction = configuration.interceptors.responses.filter(
+      c => c.matcher instanceof Function
+    );
+
+    rootConfiguration.interceptors.responses.push(...configurationsMatcherFunction, ...configurationsMatcherString);
+    return rootConfiguration;
+  }, initialValue);
+}
+
+function createResponseInterceptor(baseUrl: string, matcherConfiguration: ResponseMatcher) {
+  if (typeof matcherConfiguration.matcher === 'string') {
+    const finalPath = urljoin(baseUrl, <string>matcherConfiguration.matcher);
+    const regExp = pathToRegexp(finalPath);
+
+    return (response: AxiosResponse) => {
+      const url = response.config.url;
+
+      if (regExp.test(url)) {
+        const { schema, dataSelector } = matcherConfiguration;
+        if (dataSelector) {
+          const data = response.data[matcherConfiguration.dataSelector];
+          response.data[matcherConfiguration.dataSelector] = morphism(schema, data);
+        } else {
+          response.data = morphism(schema, response.data);
+        }
+      }
+      return response;
+    };
+  }
+  if (matcherConfiguration.matcher instanceof Function) {
+    console.log('Function Matcher');
+
+    return (response: AxiosResponse) => {
+      return response;
+    };
+  }
+  if (matcherConfiguration.matcher instanceof RegExp) {
+    console.log('RegExp Matcher');
+
+    return (response: AxiosResponse) => {
+      return response;
+    };
+  }
+}
+
+function createInterceptors(configuration: AxiosMorphismConfiguration) {
+  const { url } = configuration;
+  const { requests, responses } = configuration.interceptors;
+  const responseInterceptors = responses.map(matcher => createResponseInterceptor(url, matcher));
+  // const requestInterceptors = requests.map(() => {});
+  return { responses: responseInterceptors };
+}
+
+export function apply(client: AxiosInstance, ...configurations: AxiosMorphismConfiguration[]) {
+  configurations.forEach(configuration => {
+    const { responses } = createInterceptors(configuration);
+    responses.forEach(interceptor => {
+      client.interceptors.response.use(interceptor);
+    });
+  });
+  return client;
+}
